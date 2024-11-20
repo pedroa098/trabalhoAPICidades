e68563f5c69fcc4df8b7549c6478697e04923589
pedroa098 <166505674+pedroa098@users.noreply.github.com>
2024-11-19 21:41:24 -0300

    novo

--- htdocs/index.php
+++ htdocs/index.php
@@ -62,28 +62,33 @@
 
                     switch($airQuality) {
                         case "1":
-                            $quality = "Boa";
+                            $quality = "Ótimo";
                             $qualityClass = "quality-good";
+                            $qualityBackgroundColor = "background-color: #008000;";
                             $recommendation = "Bom para atividades ao ar livre";
                             break;
                         case "2":
-                            $quality = "Razoável";
+                            $quality = "Boa";
                             $qualityClass = "quality-fair";
+                            $qualityBackgroundColor = "background-color: #759e25;";
                             $recommendation = "Atividades ao ar livre são aceitáveis";
                             break;
                         case "3":
                             $quality = "Moderada";
                             $qualityClass = "quality-moderate";
+                            $qualityBackgroundColor = "background-color: #8a8a0a;";
                             $recommendation = "Evite atividades intensas ao ar livre";
                             break;
                         case "4":
                             $quality = "Ruim";
                             $qualityClass = "quality-poor";
+                            $qualityBackgroundColor = "background-color: #CD3333;";
                             $recommendation = "Evite atividades físicas ao ar livre";
                             break;
                         case "5":
                             $quality = "Péssima";
                             $qualityClass = "quality-very-poor";
+                            $qualityBackgroundColor = "background-color: #ff0000;";
                             $recommendation = "Evite sair de casa";
                             break;
                     }
@@ -107,20 +112,21 @@
 
             <div id="lat" style="display: none;"><?=$lat?></div>
             <div id="lon" style="display: none;"><?=$lon?></div>
+            <div>
 
-            <div class="city-box">
-                <div class="city-name">
-                    <div>Cidade</div>
-                    <h1><?=$cityName?></h1>
-                </div>
-                <div>    
-                    <div class="quality-box">
-                        <div class="left-panel">
-                            <h2>Índice de qualidade do ar: <span class="<?=$qualityClass?>"><?=$quality?></span></h2>
-                            <h3>Componentes de poluição - (em μg/m³)</h3>
+                <div class="city-box">
+                    <div class="city-name">
+                        <div>Cidade</div>
+                        <h1><?=$cityName?></h1>
+                    </div>
+                    <div class="quality-and-info">    
+                        <div class="quality-box">
+                            <div class="left-panel">
+                                <h2>Índice de qualidade do ar: <span class="<?=$qualityClass?>"><?=$quality?></span></h2>
+                                <h3>Componentes de poluição - (em μg/m³)</h3>
                                 <div class="responsive">
                                     <table class="pollution-table">
-                                        <thead>
+                                        <thead style="<?= $qualityBackgroundColor?>">
                                             <tr>
                                                 <th>PM2.5</th>
                                                 <th>PM10</th>
@@ -133,22 +139,44 @@
                                             <tr>
                                                 <td><?=$gas['pm2_5']?></td>
                                                 <td><?=$gas['pm10']?></td>
-                                                <td><?=$gas['no2']?></td>
                                                 <td><?=$gas['so2']?></td>
+                                                <td><?=$gas['no2']?></td>
                                                 <td><?=$gas['o3']?></td>
                                             </tr>
                                         </tbody>
                                     </table>
                                 </div>
                                 <div class="recommendation">
-                                    <h3>Recomendações: <?= $recommendation ?>;</h3>
+                                    <h3>Recomendações: <span class="<?=$qualityClass?>"><?= $recommendation ?></span>;</h3>
                                 </div>
                             </div>
                         </div>
+                        <div class="subtitles"> <!-- subtitles -->
+                            <div class="subtitles-box">
+                                <h2 style="margin-left: 10px;">Legenda:</h2>
+                                <div class="cores-indicacoes">
+                                    <div class="sq-good"></div><span style="margin-left: 4px;">Ótimo</span>
+                                </div>
+                                <div class="cores-indicacoes">
+                                    <div class="sq-fair"></div><span style="margin-left: 4px;">Boa</span>
+                                </div>
+                                <div class="cores-indicacoes">
+                                    <div class="sq-moderate"></div><span style="margin-left: 4px;">Moderada</span>
+                                </div>
+                                <div class="cores-indicacoes">
+                                    <div class="sq-poor"></div><span style="margin-left: 4px;">Ruim</span>
+                                </div>
+                                <div class="cores-indicacoes">
+                                    <div class="sq-very-poor"></div><span style="margin-left: 4px;">Péssima</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
                         <div class="right-panel">
                             <div id="map" style="height: 400px;margin-top:20px;position:relative"></div>
                         </div>
-                    </div>
+                    
+                </div>
             </main>
         <?php elseif(!empty($cities) && empty($air)) :?>
             <main>
@@ -157,6 +185,10 @@
         <?php elseif ($_SERVER["REQUEST_METHOD"] == "POST" && !empty($_POST['name']) && empty($cities)) :?>
             <main>
                 <div class="error-message"><?= $geoError ?></div>
+            </main>
+        <?php elseif ($_SERVER["REQUEST_METHOD"] == "POST" && empty($_POST['name'])) :?>
+            <main>
+                <div class="error-message"> Informe um nome de uma cidade para fazer a pesquisa! </div>
             </main>
         <?php endif ;?>
         </div>
